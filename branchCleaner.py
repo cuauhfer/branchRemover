5eb32671e52f96c0f674eda0f1bcd1eb01a68722
Cuauhtli Fernando <cuauh.fer.bh@gmail.com>
2022-02-02 21:06:25 -0600

    Iterative deletion and confirmation message

--- branchCleaner.py
+++ branchCleaner.py
@@ -1,7 +1,24 @@
 
 import os
+from traceback import print_list
 
-BRANCH_LIST = []
+BRANCH_LIST = ['master', 'QA', 'dev', 'miLocalBranch']
+PROTECTED_BRANCHES = ['master', 'dev', 'QA']
 
-os.system('git branch -D main')
-os.system('git branch -a')
+print('The next branches will be deleted: ')
+for branch in BRANCH_LIST:
+    print('\t - ' + branch)
+
+confirmation = input('Continue? Y/N ')
+
+if confirmation != 'Y' and confirmation != 'y':
+    exit()
+
+for branch in BRANCH_LIST:
+    if branch in PROTECTED_BRANCHES:
+        print('Branch ' + branch + ' can not be deleted')
+        continue
+
+    os.system('git branch -D ' + branch)
+    os.system('git push origin --delete ' + branch)
+    os.system('git branch -a')
